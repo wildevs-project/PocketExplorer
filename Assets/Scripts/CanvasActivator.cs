bdf074d4d6c5c9899d39da4f8473a11e6e0ae36c
sannie-beep <81695359+sannie-beep@users.noreply.github.com>
2024-12-21 18:09:23 +0800

    Update CanvasActivator.cs
    test

--- Assets/Scripts/CanvasActivator.cs
+++ Assets/Scripts/CanvasActivator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CanvasActivator : MonoBehaviour
 {
     // Start is called before the first frame update
+    //test
     public GameObject gameObject;
 
     void Start()
